fc87ba043e5dcd1ba17192dd44893eeded88aff6
Jyotideep <jyotideepjee@gmail.com>
2024-03-14 21:41:42 +0530

    added frontend

--- src/App.js
+++ src/App.js
@@ -1,57 +1,173 @@
 import axios from "axios";
 import { useState } from "react";
 
+//state -> these are objects that store values of entity which undergoes constant change in our webpage : 
 function App() {
+  //hooks -> they basically are functions that help in managing the current nature/condn of our component
+  const URL = "https://coder-stats.vercel.app";
+  const [platform, setPlatform] = useState(""); 
+  const [userName, setUsername] = useState("");
+  const [userProfile, setUserProfile] = useState(null);
 
-  const URL = "https://coder-stats.vercel.app"
-
-  const [user, setUser] = useState(null);
-  const [userData, setUserData] = useState(null);
-  const [platform, setPlatform] = useState('');
-
-  const handleSelectChange = (event)=>{
-    setPlatform(event.target.value);
+  // const increaseCount = ()=>{
+  //   setCount(count+1);
+  // }
+  const handleSelectChange = (event) =>{
+    setPlatform(event.target.value)
   }
 
   const handleInputChange = (event) =>{
-    setUser(event.target.value);
+    setUsername(event.target.value);
   }
 
-  const fetchData = async()=>{
-    try {
-      const config = {headers:{
-        "Content-Type" : "application/json",
-      }};
-      console.log(`${URL}/user/${platform}/${user}`);
-      const response = await axios.get(`${URL}/user/${platform}/${user}`, config);
-      console.log(response);
-      // Handle the response data
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      // Handle errors
+  const fetchCodeforcesProfile = async()=>{
+    try{
+    const req_url = `${URL}/user/${platform}/${userName}`;
+    const url2 = `${URL}/user/${platform}/${userName}/status`;
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  
+    const response = await axios.get(req_url,config);
+    const response2 = await axios.get(url2,config);
+
+    const status = response2.data.result;
+    // console.log(status.length);
+    const questionSet = new Set();
+    for(let i=0; i<status.length; i++){
+      if(status[i].verdict === "OK") questionSet.add(`${status[i].contestId}+${status[i].problem.index}`)
+    }
+    const solved = (questionSet.size);
+
+    const data = response.data;
+    if(data.status === "OK"){
+      const {friendOfCount, handle, maxRank, maxRating, rank, rating} = data.result[0];
+      const userData = {
+        handle,
+        maxRank,
+        maxRating,
+        rank,
+        rating,
+        friends : friendOfCount,
+        questionsSolved : solved,
+      }
+      setUserProfile(userData);
+    }
+
+    }catch(error){
+      console.log(error);
+    }
+  }
+
+  const fetchLeetcodeProfile = async()=>{
+    try{
+      const req_url = `${URL}/user/${platform}/${userName}`; 
+      const url2 =  `${URL}/user/${platform}/${userName}/rating`;
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const response = await axios.get(req_url, config);
+      const ratingRes = await axios.get(url2,config);
+
+      //rating/ranking data 
+      const ratingData = ratingRes.data.data;
+      const {userContestRanking} = ratingData;
+      const {attendedContestsCount, badge, rating} = userContestRanking;
+      // console.log(attendedContestsCount, badge, rating);
+
+      //general data
+      const data = response.data.data.matchedUser;
+      const {username, languageProblemCount, profile} = data;
+      const {ranking, userAvatar} = profile;
+      // console.log(username,languageProblemCount,ranking,userAvatar);
+      console.log({attendedContestsCount, badge, rating, ranking, userAvatar, username, languageProblemCount})
+      setUserProfile({attendedContestsCount, badge, rating, ranking, userAvatar, username, languageProblemCount})
+    }catch(error){
+      console.log(error);
+    }
+  }
+  
+  const fetchProfile = ()=>{
+    setUserProfile(null);
+    if(platform === 'codeforces'){
+      fetchCodeforcesProfile();
+    }
+    else if(platform === 'leetcode'){
+      fetchLeetcodeProfile();
+    }
+
+    else{
+      alert('Select platform');
     }
   }
 
   return (
     <div className="App">
-      <select name="platform"value={platform} onChange={handleSelectChange}>
-        <option value="">Select an option</option>
-        <option value="leetcode">Leetcode</option>
+      <select value={platform} onChange={handleSelectChange}>
+        <option value="Select platform">Select platform</option>
         <option value="codeforces">Codeforces</option>
+        <option value="leetcode">Leetcode</option>
       </select>
 
-      <p>{platform}</p>
+      <h5>{platform}</h5>
 
-      <input 
-        type="text" 
-        value={user} 
-        onChange={handleInputChange} 
+      <input
+        type="text"
+        // value={username}
         placeholder="Enter username"
+        onChange={handleInputChange}
       />
-      <p>{user}</p>
-      <button onClick={fetchData}> get </button>
-    </div>
+      <h5>{userName}</h5>
+
+      <button onClick={fetchProfile}>Get</button>
+      {/* <button onClick={increaseCount}>+</button>
+      <h5>{count}</h5> */}
+      {
+        (userProfile && platform === 'codeforces') && (
+          <div style={{backgroundColor : 'yellow', maxWidth : 200}}>
+            <div style={{'display' : 'flex', flexDirection : 'row'}}> <h5>Handle :</h5> <h5>{userProfile.handle}</h5> </div>
+            <div style={{'display' : 'flex', flexDirection : 'row'}}> <h5>Rank :</h5> <h5>{userProfile.rank}</h5> </div>
+            <div style={{'display' : 'flex', flexDirection : 'row'}}>  <h5>Rating : </h5> <h5>{userProfile.rating}</h5> </div>
+            <div style={{'display' : 'flex', flexDirection : 'row'}}>  <h5>MaxRank :</h5>  <h5>{userProfile.maxRank}</h5> </div>
+            <div style={{'display' : 'flex', flexDirection : 'row'}}> <h5>MaxRating :</h5> <h5>{userProfile.maxRating}</h5> </div>
+            <div style={{'display' : 'flex', flexDirection : 'row'}}> <h5>Friend : </h5>  <h5>{userProfile.friends}</h5></div>
+            <div style={{'display' : 'flex', flexDirection : 'row'}}> <h5>Questions Solved : </h5>  <h5>{userProfile.questionsSolved}</h5></div>
+          </div> 
+        )
+      }
+      { (userProfile && platform === 'leetcode') && (
+          <div>
+            <div style={{'display' : 'flex', flexDirection : 'row'}}> <h5>Handle :</h5> <img src={userProfile.userAvatar} alt="avatar"/> </div>
+            <div style={{'display' : 'flex', flexDirection : 'row'}}> <h5>Handle :</h5> <h5>{userProfile.username}</h5> </div>
+            <div style={{'display' : 'flex', flexDirection : 'row'}}>  <h5>Rating : </h5> <h5>{userProfile.rating}</h5> </div>
+            <div style={{'display' : 'flex', flexDirection : 'row'}}>  <h5>Badge :</h5>  <h5>{userProfile.badge.name}</h5> </div>
+            <div style={{'display' : 'flex', flexDirection : 'row'}}> <h5>Contests Attended :</h5> <h5>{userProfile.attendedContestsCount}</h5> </div>
+            {/* <div style={{'display' : 'flex', flexDirection : 'row'}}> <h5>Friend : </h5>  <h5>{userProfile.friends}</h5></div>
+            <div style={{'display' : 'flex', flexDirection : 'row'}}> <h5>Questions Solved : </h5>  <h5>{userProfile.questionsSolved}</h5></div> */}
+            <div><h5>Problems Solved</h5></div>
+            {
+              userProfile.languageProblemCount.map((item,index)=>{
+                return <div key={index} style={{'display' : 'flex', flexDirection : 'row'}}> <h5>{item.languageName} :</h5> <h5>{item.problemsSolved}</h5></div>
+              })
+            }
+          </div>
+        )
+      }
+    </div>    
   );
 }
 
+//virtual is sasta copy of original one 
+//virtual DOM -> copy of original html document convert into DOM
+//everytime a component is changed due to some interaction -> 
+//corresponding html tag is also changed
+//instead of reloading / re-rendering the entire html document
+//it compares the original doc with the virtual doc
+//virutal whatever is changed -> original 
 export default App;
